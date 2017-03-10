3b08c158da59dd7ccd12b79e08557288e90f2d22
FaintSadess <yh289436955@gmail.com>
2017-03-10 13:22:19 +0800

    Revert "add"
    This reverts commit cafe13482c0a7d8fcb36dd21411db79a93d06309.

--- src/js/souye.js
+++ src/js/souye.js
@@ -1,17 +1,18 @@
 /**
  * Created by Administrator on 2017/3/9.
  */
-import React from 'react';
-import  Public from './bottom.js'
-class Souye extends React.Component{
-    render() {
+var React = require('react');
+require('../css/init.css');
+require('../css/index.css');
+
+var Sou = React.createClass({
+    render : function () {
         return (
             <div>
-                <Public is_name="首页" />
-                <h2>暂时不知道怎么写呢</h2>
+                <h1>测试</h1>
             </div>
         )
     }
-}
+});
 
-export default  Souye;
+export default  Sou;

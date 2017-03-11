e3709eb5a972cd2954a03f095d124d09675e002a
yanghao <289436955@qq.com>
2017-03-11 14:19:21 +0800

    记录界面
    还差一个提交按钮

--- src/js/minutes.js
+++ src/js/minutes.js
@@ -4,16 +4,32 @@
 /*
 * 记录界面
 */
-
 import React from 'react';
 import Public from './bottom.js';
+
+class Content extends  React.Component {
+    render (){
+        return (
+            <div className="index_minutex">
+                <div className="content_title">
+                    <input type="text" placeholder="请输入标题"/>
+                </div>
+                <div className="content_text">
+                    <textarea name="" placeholder="请输入正文"></textarea>
+                </div>
+            </div>
+        )
+    }
+};
 
 class Minutex extends React.Component {
     render (){
         return (
             <div>
+                <div>
+                    <Content />
+                </div>
                 <Public is_name="记录" />
-                <h1>记录界面</h1>
             </div>
         )
     }
